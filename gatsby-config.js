17acc9046c4501117eecdcc0753a9a4051bf334c
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2023-02-22 22:50:52 +0200

    Add new GraphiQL query

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,11 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 module.exports = {
   /* Your site config here */
@@ -21,5 +26,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
     `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: "1luzw9h9hssv",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    }
   ]
 }
--- src/components/ImageMasonry.js
+++ src/components/ImageMasonry.js
@@ -4,43 +4,80 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import "../styles/imageMasonryGallery.css"
 
 export default function ImageMasonry({showLightbox}) {
-  const data = useStaticQuery(graphql`
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     bezbogPhotos: allFile(
+  //       filter: {extension: {regex: "/(jpg)|(png)|(jpeg)/"}, relativeDirectory: {eq: "bezbog"}}
+  //     ) {
+  //       edges {
+  //         node {
+  //           id
+  //           relativePath
+  //           childImageSharp {
+  //             gatsbyImageData(quality: 100, formats: WEBP, tracedSVGOptions: {})
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  const data2 = useStaticQuery(graphql`
     query {
-      bezbogPhotos: allFile(
-        filter: {extension: {regex: "/(jpg)|(png)|(jpeg)/"}, relativeDirectory: {eq: "bezbog"}}
-      ) {
+      allContentfulAlbum(filter: {nameOfAlbum: {eq: "Bezbog"}}) {
         edges {
           node {
             id
-            relativePath
-            childImageSharp {
-              gatsbyImageData(quality: 100, formats: WEBP, tracedSVGOptions: {})
+            nameOfAlbum
+            album {
+              gatsbyImageData(quality: 80, formats: WEBP, placeholder: BLURRED)
+              id
             }
           }
         }
       }
     }
-  `)
-
+  `);
+  console.log(data2);
   return (
+    // <>
+    //   <div className="imageMasonryGallery">
+    //     {data.bezbogPhotos.edges.map(image => (
+    //       <div
+    //         className="masonryImage"
+    //         onClick={() =>
+    //           showLightbox(image.node.childImageSharp.gatsbyImageData)
+    //         }
+    //         key={image.node.id}
+    //       >
+    //         <GatsbyImage
+    //           image={image.node.childImageSharp.gatsbyImageData}
+    //           alt={image.node.relativePath}
+    //         />
+    //       </div>
+    //     ))}
+    //     <div>images here</div>
+    //   </div>
+    // </>
+    // <>
     <>
-      <div className="imageMasonryGallery">
-        {data.bezbogPhotos.edges.map(image => (
-          <div
-            className="masonryImage"
-            onClick={() =>
-              showLightbox(image.node.childImageSharp.gatsbyImageData)
-            }
-            key={image.node.id}
-          >
-            <GatsbyImage
-              image={image.node.childImageSharp.gatsbyImageData}
-              alt={image.node.relativePath}
-            />
-          </div>
-        ))}
-        <div>images here</div>
-      </div>
-    </>
+    <div className="imageMasonryGallery">
+      {data2.allContentfulAlbum.edges[0].node.album.map(image => (
+        <div
+          className="masonryImage"
+          onClick={() =>
+            showLightbox(image.gatsbyImageData)
+          }
+          key={image.id}
+        >
+          <GatsbyImage
+            image={image.gatsbyImageData}
+            alt={image.id}
+          />
+        </div>
+      ))}
+      <div>images here</div>
+    </div>
+  </>
   )
 }
