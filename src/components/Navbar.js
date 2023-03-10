838bae3e3a556d060ae0768bf7e64b83392818fb
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2023-03-10 23:33:05 +0200

    Add responsive img style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,32 +4,40 @@ import "../styles/navbar.css"
 import "../styles/hamburgers.css"
 
 export default function Navbar() {
-  const [hamActive, setActive] = useState(false);
+  const [hamActive, setActive] = useState(false)
 
   const toggleHam = () => {
-    setActive((hamActive) => !hamActive)
+    setActive(hamActive => !hamActive)
   }
 
   return (
     <nav>
-      <div className={hamActive?"homeLogo noDisplay":"homeLogo"}>
-        <Link to="/">
-          Mihaylo Dimo
-        </Link>
+      <div className="navWrapper">
+        <div className={hamActive ? "homeLogo noDisplay" : "homeLogo"}>
+          <Link to="/">Mihaylo Dimo</Link>
+        </div>
+        <div className={hamActive ? "navLinks" : "navLinks noDisplay"}>
+          {/* <Link to="/">Home</Link> */}
+          <Link to="/portraits">Portraits</Link>
+          <Link to="/landscapes">Landscapes</Link>
+          <Link to="/events">Events</Link>
+          <Link to="/albums">Albums</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <button
+          className={
+            hamActive
+              ? "hamburger hamburger--arrowalt ham-is-active"
+              : "hamburger hamburger--arrowalt"
+          }
+          type="button"
+          onClick={toggleHam}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
       </div>
-      <div className={hamActive?"navLinks":"navLinks noDisplay"}>
-        {/* <Link to="/">Home</Link> */}
-        <Link to="/portraits">Portraits</Link>
-        <Link to="/landscapes">Landscapes</Link>
-        <Link to="/events">Events</Link>
-        <Link to="/albums">Albums</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <button className={hamActive?"hamburger hamburger--arrowalt ham-is-active":"hamburger hamburger--arrowalt"} type="button" onClick={toggleHam}>
-        <span className="hamburger-box">
-          <span className="hamburger-inner"></span>
-        </span>
-      </button>
     </nav>
   )
 }
